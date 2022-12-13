7393dd1629c82c518a4c47f6c8a5170473bbfa8f
Tarun <testinggyan.tg@gmail.com>
2022-12-13 19:27:47 +0530

    Reverse a string

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+public static void reverseString(String str){
+    int length=str.length();
+
+    String res="";
+    int index=length-1;
+
+    while(index>=0){
+       res=res+str.charAt(index);
+       index--;
+    }
+
+    System.out.println(res);
+}
+
+    public static void main(String[] args) {
+        
+
+        String name="Tarun";
+
+        reverseString(name);
+    }
+    
+}
